7333c705c71788dcf128c6853a4773ed0e99ccca
William Bonitz <wbnhw3@gmail.com>
2016-08-04 18:30:59 -0700

    HarryPotter houses

--- src/Houses1.java
+++ src/Houses1.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import org.teachingextensions.logo.Tortoise;
 
 public class Houses1 {
@@ -6,27 +8,46 @@ public class Houses1 {
 		Tortoise.setY(375);
 		Tortoise.show();
 		Tortoise.setSpeed(10);
-		drawHouse(300);		
-		drawHouse(200);
-		drawHouse(100);
-		drawHouse(50);
-		drawHouse(25);
-		drawHouse(10);
-		drawHouse(5);
-		
-
+		drawHouse("big","green");
+		drawHouse("medium","blue");
+		drawHouse("small","red");
 	}
 
-	public static void drawHouse(int height) {
+	public static void drawHouse(String height, String color) {
+		int pixel = 0;
 
+		System.out.println(height);
+		if (color.equals("green")) {
+			Tortoise.penDown();
+			Tortoise.setPenColor(Color.green);
+		}
+		if (color.equals("blue")) {
+			Tortoise.penDown();
+			Tortoise.setPenColor(Color.blue);
+		}
+		if (color.equals("red")) {
+			Tortoise.penDown();
+			Tortoise.setPenColor(Color.red);
+		}
+		if (height.equals("big")) {
+			pixel = 250;
+		}
+		if (height.equals("medium")) {
+			pixel = 120;
+		}
+		if (height.equals("small")) {
+			pixel = 60;
+		}
 		for (int i = 0; i < 3; i++) {
-			Tortoise.move(height);
+			Tortoise.move(pixel);
 			Tortoise.turn(90);
+
 		}
 		for (int h = 0; h < 2; h++) {
 			Tortoise.turn(90);
 		}
 		Tortoise.move(50);
-       Tortoise.turn(-90);
+		Tortoise.turn(-90);
+
 	}
 }
