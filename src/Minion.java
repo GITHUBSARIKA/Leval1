ccad6bec4447a8763dca5f6060ba7750bd715eb3
GITHUBSARIKA <league@WorthyMini-6.attlocal.net>
2017-04-06 18:30:52 -0700

    minions

--- src/Minion.java
+++ src/Minion.java
@@ -1,4 +1,16 @@
 
 public class Minion {
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
+
+	public Minion(int eyes, int color, String name, String master) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.eyes = eyes;
+		this.color = color;
+		this.master = master;
+	}
 
 }
